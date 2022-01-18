ae3f4569eb6846c00d4c068bc545d4890f100cc9
immingheng <im.mingheng@gmail.com>
2022-01-18 09:17:25 +0800

    Initial commit for Springboot maven project with relevant dependencies + jedis and JSON-P

---
+++ SSFAssessment/src/test/java/ibf2021/SSFAssessment/SsfAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf2021.SSFAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
